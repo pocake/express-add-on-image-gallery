0ee50b00039f6897e4302480e8ee5cd3d8b5de35
uto-usui <me@uto-usui.com>
2024-07-02 10:43:52 +0900

    Update icon titles in Icon.js
    In this commit, titles of icons in Icon.js have been updated to include a hyphen between 'Icon' and its number for clarity. This change affects all icon titles in the file.

--- src/assets/imageData/Icon.js
+++ src/assets/imageData/Icon.js
@@ -1,217 +1,217 @@
 export const Icon = [
   {
     'imageUrl': './images/Icon/Icon1.png',
-    'title': 'Icon1',
+    'title': 'Icon-1',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon10.png',
-    'title': 'Icon10',
+    'title': 'Icon-10',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon11.png',
-    'title': 'Icon11',
+    'title': 'Icon-11',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon12.png',
-    'title': 'Icon12',
+    'title': 'Icon-12',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon13.png',
-    'title': 'Icon13',
+    'title': 'Icon-13',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon14.png',
-    'title': 'Icon14',
+    'title': 'Icon-14',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon15.png',
-    'title': 'Icon15',
+    'title': 'Icon-15',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon16.png',
-    'title': 'Icon16',
+    'title': 'Icon-16',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon17.png',
-    'title': 'Icon17',
+    'title': 'Icon-17',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon18.png',
-    'title': 'Icon18',
+    'title': 'Icon-18',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon19.png',
-    'title': 'Icon19',
+    'title': 'Icon-19',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon2.png',
-    'title': 'Icon2',
+    'title': 'Icon-2',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon20.png',
-    'title': 'Icon20',
+    'title': 'Icon-20',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon21.png',
-    'title': 'Icon21',
+    'title': 'Icon-21',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon22.png',
-    'title': 'Icon22',
+    'title': 'Icon-22',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon23.png',
-    'title': 'Icon23',
+    'title': 'Icon-23',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon24.png',
-    'title': 'Icon24',
+    'title': 'Icon-24',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon25.png',
-    'title': 'Icon25',
+    'title': 'Icon-25',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon26.png',
-    'title': 'Icon26',
+    'title': 'Icon-26',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon27.png',
-    'title': 'Icon27',
+    'title': 'Icon-27',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon28.png',
-    'title': 'Icon28',
+    'title': 'Icon-28',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon29.png',
-    'title': 'Icon29',
+    'title': 'Icon-29',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon3.png',
-    'title': 'Icon3',
+    'title': 'Icon-3',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon30.png',
-    'title': 'Icon30',
+    'title': 'Icon-30',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon31.png',
-    'title': 'Icon31',
+    'title': 'Icon-31',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon32.png',
-    'title': 'Icon32',
+    'title': 'Icon-32',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon33.png',
-    'title': 'Icon33',
+    'title': 'Icon-33',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon34.png',
-    'title': 'Icon34',
+    'title': 'Icon-34',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon35.png',
-    'title': 'Icon35',
+    'title': 'Icon-35',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon36.png',
-    'title': 'Icon36',
+    'title': 'Icon-36',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon37.png',
-    'title': 'Icon37',
+    'title': 'Icon-37',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon38.png',
-    'title': 'Icon38',
+    'title': 'Icon-38',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon39.png',
-    'title': 'Icon39',
+    'title': 'Icon-39',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon4.png',
-    'title': 'Icon4',
+    'title': 'Icon-4',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon40.png',
-    'title': 'Icon40',
+    'title': 'Icon-40',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon41.png',
-    'title': 'Icon41',
+    'title': 'Icon-41',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon42.png',
-    'title': 'Icon42',
+    'title': 'Icon-42',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon43.png',
-    'title': 'Icon43',
+    'title': 'Icon-43',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon5.png',
-    'title': 'Icon5',
+    'title': 'Icon-5',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon6.png',
-    'title': 'Icon6',
+    'title': 'Icon-6',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon7.png',
-    'title': 'Icon7',
+    'title': 'Icon-7',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon8.png',
-    'title': 'Icon8',
+    'title': 'Icon-8',
     'assetType': 'Icon'
   },
   {
     'imageUrl': './images/Icon/Icon9.png',
-    'title': 'Icon9',
+    'title': 'Icon-9',
     'assetType': 'Icon'
   }
 ];
